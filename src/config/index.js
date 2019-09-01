c1e8b7113330328fcfd723cded4c3277110597e5
Eduardo Compiani <eduardovieiracompiani@gmail.com>
2019-09-01 13:45:07 -0300

    Codigo desenvolvido

---
+++ src/config/index.js
@@ -0,0 +1,9 @@
+export default {
+  apiKey: 'AIzaSyAhgcKNv-8tFpYvay7FRqI6ulkqP-oiWyY',
+  authDomain: 'workshopvue.firebaseapp.com',
+  databaseURL: 'https://workshopvue.firebaseio.com',
+  projectId: 'workshopvue',
+  storageBucket: 'workshopvue.appspot.com',
+  messagingSenderId: '272083094806',
+  appId: '1:272083094806:web:5ee15a77c9081f3c'
+}
